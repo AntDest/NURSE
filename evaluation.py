875cebc2d0fd9f6522efb73b6b0a5e1f9be69b0a
Antoine d'Estalenx <antdestalenx@gmail.com>
2021-03-17 16:06:35 +0100

    Allow to delete all results with a flag

--- evaluation.py
+++ evaluation.py
@@ -3,6 +3,7 @@ import subprocess
 import os
 import time
 import json
+import argparse
 capture_folder = "evaluation/captures/"
 capture_files = {}
 capture_files["benign"] = [capture_folder + "normal/" + c for c in os.listdir(capture_folder+"normal/")]
@@ -10,16 +11,22 @@ capture_files["malicious"] = [capture_folder + "malicious/" + c for c in os.list
 
 
 
-def evaluate_file(capture_file):
+def evaluate_file(label, capture_file):
+    print("EVALUATION OF ", capture_file)
     t1 = time.time()
-    output_file = "evaluation/results/" + capture_file.split("/")[-1].split(".pcap")[0] + ".json"
+    output_file = "evaluation/results/" + label + "_" + capture_file.split("/")[-1].split(".pcap")[0] + ".json"
     if os.path.exists(output_file):
         print(output_file, " exists")
+        return
+
+
     subprocess.run(
         ["python", "main.py", "--offline", capture_file, "--output", output_file],
         check=False,
         # stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL,
     )
+
+
     t2 = time.time()
     count = 0
     while not os.path.exists(output_file) and count < 5:
@@ -37,18 +44,22 @@ def evaluate_file(capture_file):
                 \n\tAlerts: {len(data['alerts'])}\
                 \n\tPackets: {n_packets}, Time: {t2-t1:.2f}, (Packets per second: {packets_per_second:.1f})"
             )
-        if len(alert_list) > 0:
-            print(alert_list[:5])
-
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--reset', type=bool, nargs="?", const=True, default=False, help='Use it to delete all results')
+    args = parser.parse_args()
+    if args.reset:
+        for f_result in os.listdir("evaluation/results/"):
+            os.remove("evaluation/results/" + f_result)
     try:
         processes = []
         starttime = time.time()
-        for c in capture_files["benign"] + capture_files["malicious"]:
-            p = multiprocessing.Process(target=evaluate_file, args=(c,))
-            processes.append(p)
-            p.start()
+        for label in capture_files:
+            for c in capture_files[label]:
+                p = multiprocessing.Process(target=evaluate_file, args=(label, c))
+                processes.append(p)
+                p.start()
     except KeyboardInterrupt:
         print("Ending")
     finally:
